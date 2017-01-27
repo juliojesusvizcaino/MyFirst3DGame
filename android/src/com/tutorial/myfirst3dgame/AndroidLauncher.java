6359ff7dc08503a6cb5f1bcc88f77df22718bf5a
Julio Vizcaíno Molina <juliojesusvizcaino@gmail.com>
2017-01-27 07:18:53 +0100

    Initial commit

---
+++ android/src/com/tutorial/myfirst3dgame/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.tutorial.myfirst3dgame;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.tutorial.myfirst3dgame.MyFirst3DGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MyFirst3DGame(), config);
+	}
+}
---
+++ desktop/src/com/tutorial/myfirst3dgame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.tutorial.myfirst3dgame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.tutorial.myfirst3dgame.MyFirst3DGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyFirst3DGame(), config);
+	}
+}
