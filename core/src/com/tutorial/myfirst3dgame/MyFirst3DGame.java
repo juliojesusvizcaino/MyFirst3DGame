fddb1ba0ac471cf7269526e3cecad1c63f707887
Julio Vizcaíno Molina <juliojesusvizcaino@gmail.com>
2017-01-28 04:37:15 +0100

    Add blocks and invaders

--- core/src/com/tutorial/myfirst3dgame/MyFirst3DGame.java
+++ core/src/com/tutorial/myfirst3dgame/MyFirst3DGame.java
@@ -3,64 +3,86 @@ package com.tutorial.myfirst3dgame;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetManager;
-import com.badlogic.gdx.assets.loaders.ModelLoader;
-import com.badlogic.gdx.graphics.*;
-import com.badlogic.gdx.graphics.g3d.*;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.PerspectiveCamera;
+import com.badlogic.gdx.graphics.g3d.Environment;
+import com.badlogic.gdx.graphics.g3d.Model;
+import com.badlogic.gdx.graphics.g3d.ModelBatch;
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
 import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
 import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
-import com.badlogic.gdx.graphics.g3d.loader.ObjLoader;
 import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
-import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.Array;
 
 public class MyFirst3DGame extends ApplicationAdapter {
 	public PerspectiveCamera cam;
 	public ModelBatch modelBatch;
-	public Model model, modelImported;
-	public ModelInstance instance, instanceImported;
 	public Environment environment;
 	public CameraInputController camController;
 	public AssetManager assets;
 	public Array<ModelInstance> instances = new Array<ModelInstance>();
 	public boolean loading;
+
+	public Array<ModelInstance> blocks = new Array<ModelInstance>();
+	public Array<ModelInstance> invaders = new Array<ModelInstance>();
+	public ModelInstance ship;
+	public ModelInstance space;
+
+	public void setCam () {
+        cam = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        cam.position.set(0, 7f, 10f);
+        cam.lookAt(0, 0,0);
+        cam.near = 1f;
+        cam.far = 300f;
+        cam.update();
+
+        camController = new CameraInputController(cam);
+        Gdx.input.setInputProcessor(camController);
+    }
 	
 	@Override
 	public void create () {
 	    modelBatch = new ModelBatch();
-
-	    cam = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-	    cam.position.set(10f, 10f, 10f);
-	    cam.lookAt(0, 0,0);
-	    cam.near = 1f;
-	    cam.far = 300f;
-	    cam.update();
-
-        ModelBuilder modelBuilder = new ModelBuilder();
-        model = modelBuilder.createBox(5f, 5f, 5f,
-                new Material(ColorAttribute.createDiffuse(Color.GREEN)),
-                VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal);
-        instances.add(new ModelInstance(model));
-
         environment = new Environment();
         environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));
         environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f));
 
-        camController = new CameraInputController(cam);
-        Gdx.input.setInputProcessor(camController);
+        setCam();
 
         assets = new AssetManager();
-        assets.load("data/ship/ship.g3db", Model.class);
+        assets.load("data/ship.obj", Model.class);
+        assets.load("data/block.obj", Model.class);
+        assets.load("data/invader.obj", Model.class);
+        assets.load("data/spacesphere.obj", Model.class);
         loading = true;
 	}
 
 	private void doneLoading() {
-	    Model ship = assets.get("data/ship/ship.g3db", Model.class);
-	    for (float x = 0f; x <= 6f; x += 2f) {
-	        for (float z = 0f; z <= 4f; z += 2f) {
-                ModelInstance shipInstance = new ModelInstance(ship, x, 3f, z);
-                instances.add(shipInstance);
+	    ship = new ModelInstance(assets.get("data/ship.obj", Model.class));
+	    ship.transform.setToRotation(Vector3.Y, 180).trn(0,0,6f);
+	    instances.add(ship);
+
+	    Model blockModel = assets.get("data/block.obj", Model.class);
+	    for (float x = -5f; x <=5f; x+= 2f) {
+	        ModelInstance block = new ModelInstance(blockModel);
+            block.transform.setToTranslation(x, 0, 3f);
+            instances.add(block);
+            blocks.add(block);
+        }
+
+        Model invaderModel = assets.get("data/invader.obj", Model.class);
+	    for (float x = -5f; x <= 5f; x += 2f) {
+	        for (float z = -8f; z <= 0; z += 2f) {
+	            ModelInstance invader = new ModelInstance(invaderModel);
+	            invader.transform.setToTranslation(x, 0, z);
+	            instances.add(invader);
+	            invaders.add(invader);
             }
         }
+
+        space = new ModelInstance(assets.get("data/spacesphere.obj", Model.class));
+
 	    loading = false;
     }
 
@@ -76,13 +98,14 @@ public class MyFirst3DGame extends ApplicationAdapter {
 
 		modelBatch.begin(cam);
 		modelBatch.render(instances, environment);
+		if (space != null)
+		    modelBatch.render(space);
 		modelBatch.end();
 	}
 	
 	@Override
 	public void dispose () {
 	    modelBatch.dispose();
-	    model.dispose();
 	    instances.clear();
 	    assets.dispose();
 	}
